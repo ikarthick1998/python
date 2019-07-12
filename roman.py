40724286fbe7e9291d75a5fbb452d3d8b5468caa
MadhanGP <51233268+MadhanGP@users.noreply.github.com>
2019-07-12 17:38:18 +0530

    Update roman.py

--- roman.py
+++ roman.py
@@ -1,4 +1,3 @@
-give=int(input())
-dic={1:'I',2:'II',3:'III',4:'IV',5:'V',6:'VI',7:'VII',8:'VII',9:'XI',10:'X',11:'XI',12:'XII',13:'XIII',14:'XIV',15:'XV',16:'XVI',17:'XVII',18:'XVIII',19:'XIX',20:'XX'}
-if give>=0 & give<21:
-  print(dic[give])
+give=input("")
+dic={'I':1,'II':2,'III':3,'IV':4,'V':5,'VI':6,'VII':7,'VIII':8,'IX':9,'X':10,'XI':11,'XII':12,'XIII':13,'XIV':14,'XV':15,'XVI':16,'XVII':17,'XIX':19,'XX':20}
+print(dic[give])
